44dbd836a26203b83786525cb7d07c4da6b3ff87
Priya Gupta <priya.s.gupta@jpmorgan.com>
2023-11-11 15:44:29 -0800

    Added a singleton module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-const container = document.querySelector('#container');
-container.innerHTML = 'Hello World';
+//Singleton Header Module
+const headerSingleton = function(){
+    const addHeader = (parentElement) => {
+        const headerElement = document.createElement('div');
+        headerElement.innerHTML = 'Header Element';
+        parentElement.appendChild(headerElement);
+    }
+
+    return {
+        addHeader
+    }
+}()
+
+headerSingleton.addHeader(document.querySelector('#header'));
+
+const sections = document.querySelectorAll('#container');
+
+
+
