e7cfbd39a71e9d6e3761f4e0173acf00b5677a00
Priya Gupta <priya.s.gupta@jpmorgan.com>
2023-11-11 15:19:22 -0800

    Created a simple Module using an IIFE

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-const container = document.querySelector('#container');
-container.innerHTML = 'Hello World';
+const bodyElement = document.querySelector('body');
+
+/* Creating a module created using iife
+This is an ananymous function that adds a header element to a given parent Element
+Adavntages of IIFE
+- wraps the implementation details inside the IIFE
+- There is no leakage of information in the global scope
+- Exposes a clean interface
+- Singleton pattern where the function will be executed only once
+*/
+(function(parentElement){
+    const headerElement = document.createElement('div');
+    headerElement.innerHTML = 'Header Element' ;
+    parentElement.appendChild(headerElement);
+})(bodyElement)
