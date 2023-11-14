25d5beb08fa3025401d0bd07fbae850b64626717
Priya Gupta <priya.s.gupta@jpmorgan.com>
2023-11-14 11:29:28 -0800

    Can't remember where I left off..so yeah

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //Singleton Header Module
-const headerSingleton = function(){
+const headerSingletonModule = function () {
+    let count = 0;
     const addHeader = (parentElement) => {
+        ++count;
         const headerElement = document.createElement('div');
-        headerElement.innerHTML = 'Header Element';
+        headerElement.innerHTML = `Header Element ${count}`;
         parentElement.appendChild(headerElement);
     }
 
@@ -11,9 +13,77 @@ const headerSingleton = function(){
     }
 }()
 
-headerSingleton.addHeader(document.querySelector('#header'));
+//Singleton Header Module application
+headerSingletonModule.addHeader(document.querySelector('#header'));
 
-const sections = document.querySelectorAll('#container');
 
+
+const sections = document.querySelectorAll('.section');
+sections.forEach((sectionElement) => {
+    // sectionModule.addSection(sectionElement)
+    headerSingletonModule.addHeader(sectionElement)
+})
+
+
+const sectionModule = function () {
+    function Sections(){
+        this.count = 0;
+        this.innerHTMLText = `Section element ${this.count}`;
+    }
+
+    return
+
+}()
+
+/*
+
+    <Section />
+
+    function Section(){
+        this.index = 1;
+
+        this.element = document.createElement('div');
+        this.element.innerHTML = 'Hello Section ${this.index}'
+    }
+
+    Section.prototype.addElement = function(parentElement){
+        parentElement.appendChild(this.element);
+    }
+
+    const section1 = new Section();
+    section1.addElement(parentElement1);
+
+    const section2 = new Section();
+    section2.addElement(parentElement2);
+*/
+
+
+/*
+
+    const sectionModule = function(){
+    let count = 0;
+    function Section(){
+        this.index = 1;
+
+        this.element = document.createElement('div');
+        this.element.innerHTML = 'Hello Section ${this.index}'
+    }
+
+    Section.prototype.addElement = function(parentElement){
+        parentElement.appendChild(this.element);
+    }
+
+    const createObj = () => {
+        returns Object.assign({}, Section)
+    }
+}()
+
+
+
+
+
+
+
+*/
 
 
